92b3ff03ed563018c06753953f9c410b6b5e3034
CHardy <christianrh2000@hotmail.com>
2022-08-01 09:26:32 -0300

    2022-08-01 09:26 hs

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from "react-router-dom";
 import MyHeader from "./components/general/MyHeader";
 import MyFooter from "./components/general/MyFooter";
 import App from './App';
@@ -8,6 +9,7 @@ import App from './App';
 //   const linkElement = screen.getByText(/learn react/i);
 //   expect(linkElement).toBeInTheDocument();
 // });
+describe("<Searcher />", () => {
 
 it("Debe renderizarse los componentes (App, MyHeader y MyFooter)", async () => {
   // Arrange
@@ -19,3 +21,33 @@ it("Debe renderizarse los componentes (App, MyHeader y MyFooter)", async () => {
   expect(screen.getByRole('MyHeader')).toBeInTheDocument()
   expect(screen.getByRole('MyFooter')).toBeInTheDocument()
 })
+
+// app.test.js
+it("clicking filter links updates product query params", () => {
+  let testHistory, testLocation;
+  render(
+    <MemoryRouter initialEntries={["/my/initial/route"]}>
+      <App />
+      <Route
+        path="*"
+        render={({ history, location }) => {
+          testHistory = history;
+          testLocation = location;
+          return null;
+        }}
+      />
+    </MemoryRouter>,
+    node
+  );
+
+  act(() => {
+    // example: click a <Link> to /products?id=1234
+  });
+
+  // assert about url
+  expect(testLocation.pathname).toBe("/products");
+  const searchParams = new URLSearchParams(testLocation.search);
+  expect(searchParams.has("id")).toBe(true);
+  expect(searchParams.get("id")).toEqual("1234");
+});
+});
--- src/pages/SearcherNewsPage.test.jsx
+++ src/pages/SearcherNewsPage.test.jsx
@@ -1,9 +1,13 @@
 import SearcherNewsPage from "./SearcherNewsPage";
-import { render, screen, waitFor } from '@testing-library/react';
+import Searcher from "../components/general/Searcher";
+import NewsList from "../components/news/NewsList";
+import { render, screen, waitFor,act,fireEvent } from '@testing-library/react';
+
+const onSearchMock = jest.fn();
 
 describe('<SearcherNewsPage />', () => {
 
-    it("Debe renderizarse ", async () => {
+    it("Debe renderizarse <SearcherNewsPage /> ", async () => {
         // Arrange
         // Act
         render(<SearcherNewsPage />)
@@ -11,5 +15,30 @@ describe('<SearcherNewsPage />', () => {
         expect(screen.getByRole('searcher-news-page')).toBeInTheDocument()
     })
     
-    
+        // loading (Duda aqui... Estaria bien?)
+    it("Debe mostrar Loading mientras espero respuesta del servicio", async () => {
+        // Arange
+        // Act
+        render(<NewsList busqueda="bitcoin"/>);
+        // Assert
+        expect(screen.getByRole('progressbar')).toBeInTheDocument()
+    })
+
+    it("Debe aparecer una lista de noticias", async () => {
+        // Arrange
+        // Act
+        render(<SearcherNewsPage />);
+        const searcherInput = screen.getByRole('textbox');
+        const searcherButton = screen.getByRole('button');
+
+        act(() => {
+            fireEvent.change(searcherInput, { target: { value: 'test' }});
+            fireEvent.click(searcherButton);
+            //expect(onSearch).toHaveBeenCalled();
+        });
+             await waitFor(() => {
+            expect(screen.getByRole('news-list')).toBeInTheDocument();
+        });
+    })
+
 });
