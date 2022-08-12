c9432af2d8a7f1688bb292a8b88b2d981a5b1e2e
Christian H <christianrh2000@hotmail.com>
2022-08-11 21:27:11 -0300

    123456

--- src/components/news/NewsItem/index.jsx
+++ src/components/news/NewsItem/index.jsx
@@ -13,14 +13,14 @@ const NewsItem = ({ title, description, publishedAt, urlToImage,source,url,image
 
     return (
         <article className="nl-box" onClick={handleClickOpenSource} role="article"> 
+            <div className="nl-box-image">
+                <img src={urlToImage || image} alt="" className="img-fluid"/>
+            </div>
             <div className="nl-box-content">
                 <h1>{title}</h1>
                 <p>{description}</p>
                 <span>Publicado el: {fechaPublicacion} a las {horaPublicacion} hs.</span>
                 <span>Fuente: <button className="source">{source.name}</button></span>
-            </div>
-            <div className="nl-box-image">
-                <img src={urlToImage || image} alt="" className="img-fluid"/>
             </div>
         </article>
     )
